2ded4792ee054f18561e3645040dabe844f626a2
Anatolii Yatsuk <tolikyatsuk@gmail.com>
2025-10-14 15:59:36 +0300

    Revert custom queries changes

--- airbyte-integrations/connectors/source-google-ads/unit_tests/conftest.py
+++ airbyte-integrations/connectors/source-google-ads/unit_tests/conftest.py
@@ -18,25 +18,25 @@ from airbyte_cdk.test.catalog_builder import CatalogBuilder
 from airbyte_cdk.test.state_builder import StateBuilder
 
 
-def _get_manifest_path() -> Path:
-    # TODO: uncomment once migrated to manifest-only
-    # source_declarative_manifest_path = Path("/airbyte/integration_code/source_declarative_manifest")
-    # if source_declarative_manifest_path.exists():
-    #     return source_declarative_manifest_path
+# TODO: uncomment once migrated to manifest-only
+# def _get_manifest_path() -> Path:
+#     source_declarative_manifest_path = Path("/airbyte/integration_code/source_declarative_manifest")
+#     if source_declarative_manifest_path.exists():
+#         return source_declarative_manifest_path
+#     return Path(__file__).parent.parent
 
-    return Path(__file__).parent.parent / "source_google_ads"
 
-
-_SOURCE_FOLDER_PATH = _get_manifest_path()
-_YAML_FILE_PATH = "manifest.yaml"
+# _SOURCE_FOLDER_PATH = _get_manifest_path()
+# _YAML_FILE_PATH = _SOURCE_FOLDER_PATH / "manifest.yaml"
 
 # sys.path.append(str(_SOURCE_FOLDER_PATH))  # to allow loading custom components
 
 
 def get_source(config, state=None) -> YamlDeclarativeSource:
-    catalog = CatalogBuilder().build()
-    state = StateBuilder().build() if not state else state
-    return YamlDeclarativeSource(path_to_yaml=str(_SOURCE_FOLDER_PATH / _YAML_FILE_PATH), catalog=catalog, config=config, state=state)
+    # catalog = CatalogBuilder().build()
+    # state = StateBuilder().build() if not state else state
+    # return YamlDeclarativeSource(path_to_yaml=str(_SOURCE_FOLDER_PATH / _YAML_FILE_PATH), catalog=catalog, config=config, state=state)
+    return SourceGoogleAds(config, None, None)
 
 
 def find_stream(stream_name, config, state=None):
